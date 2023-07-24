71ffb759c37483a709c891b89a386e3f05707ad7
vssavin <savin1801@gmail.com>
2023-07-24 13:47:09 +0300

    init.sql - admin insert fix

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -18,3 +18,8 @@ create table IF NOT EXISTS sys_user(
  create_time timestamp not null,
  update_time timestamp not null
  );
+
+ insert into sys_user(id, username, salt, password, phone, dept_id, dept_name, real_name, nick_name, email, status,
+  sex, deleted, create_id, update_id, create_where, create_time, update_time)
+ values('1', 'admin', '123', 'e342efe0633ca0940a8e4805b047ff1c', '8-800', '000', 'dept_name', 'admin', 'admin', 'e@e.com',
+  0, 0, 1, 'create_id', 'update_id', 0, '2005-01-01 00:00:00', '2009-01-01 00:00:00');
